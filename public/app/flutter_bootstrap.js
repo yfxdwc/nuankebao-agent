13516148e2ae0c65c3785ef5349f5240b8250afa
yfxdwc <yfxdwcs@gmail.com>
2026-09-22 13:38:43 +0000

    [pi] 全部完成。4 个 commit，4 个 slice 全部落地并验证。

--- public/app/flutter_bootstrap.js
+++ public/app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "543232348"
+    serviceWorkerVersion: "4042214503"
   }
 });
--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "91221be3ac73e06b2ab3244e9cd31
 "index.html": "3ef54a0baaba4c4523e88db6b075ee6b",
 "/": "3ef54a0baaba4c4523e88db6b075ee6b",
 "manifest.json": "8ea3427efe29e7ed385451aab714dd48",
-"flutter_bootstrap.js": "a8e069445202342e4b68e3f9f2da54cd",
+"flutter_bootstrap.js": "c31e73d65bb1a3e6dcb41d40d3dbeacf",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.json": "019bb0727a026e2f9093806d14091d59",
 "assets/packages/fluttertoast/assets/toastify.js": "56e2c9cedd97f10e7e5f1cebd85d53e3",
@@ -35,7 +35,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "91221be3ac73e06b2ab3244e9cd31
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
 "version.json": "f1676a0ee7cf8cc9b88ff6ed33968038",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "4a31e83a1af63bd381d951ade6643549"};
+"main.dart.js": "3804aca2023288f4533e33bdd4462627"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
