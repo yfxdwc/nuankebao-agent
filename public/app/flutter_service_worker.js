e83215722fdd4b215cd18f51e48c82cf1b1e495f
yfxdwc <yfxdwcs@gmail.com>
2026-09-23 02:55:09 +0000

    [pi] Commit `bf5dd77` 已落定。

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -10,7 +10,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "91221be3ac73e06b2ab3244e9cd31
 "index.html": "50e41cee9605351cd7ac8a0ad1c8f8d1",
 "/": "50e41cee9605351cd7ac8a0ad1c8f8d1",
 "manifest.json": "49e2a8f16231b988d6668550c936fc45",
-"flutter_bootstrap.js": "17c50c88c7bab00cae64f2a046382339",
+"flutter_bootstrap.js": "e41135484d5a1483d199c365cc774454",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.json": "019bb0727a026e2f9093806d14091d59",
 "assets/packages/fluttertoast/assets/toastify.js": "56e2c9cedd97f10e7e5f1cebd85d53e3",
@@ -35,7 +35,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "91221be3ac73e06b2ab3244e9cd31
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
 "version.json": "f1676a0ee7cf8cc9b88ff6ed33968038",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "1ee5429645f5c55b3bd2d4cd67624ace"};
+"main.dart.js": "beda152346605e8ffa4cac6fb09bbe87"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
