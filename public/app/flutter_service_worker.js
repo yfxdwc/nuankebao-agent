304bc73ab6dd0407e160480413c9dec4f69268aa
yfxdwc <yfxdwcs@gmail.com>
2026-09-23 02:34:34 +0000

    [pi] ## 全部完成 ✅

--- public/app/flutter_service_worker.js
+++ public/app/flutter_service_worker.js
@@ -10,14 +10,14 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "91221be3ac73e06b2ab3244e9cd31
 "index.html": "3ef54a0baaba4c4523e88db6b075ee6b",
 "/": "3ef54a0baaba4c4523e88db6b075ee6b",
 "manifest.json": "8ea3427efe29e7ed385451aab714dd48",
-"flutter_bootstrap.js": "c31e73d65bb1a3e6dcb41d40d3dbeacf",
+"flutter_bootstrap.js": "772f45f79d559d1f107dd1f7d234d8fd",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.json": "019bb0727a026e2f9093806d14091d59",
 "assets/packages/fluttertoast/assets/toastify.js": "56e2c9cedd97f10e7e5f1cebd85d53e3",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/NOTICES": "9cc2a1feee8cff55dd9f6ad8e517ec90",
-"assets/fonts/MaterialIcons-Regular.otf": "e0fd3cc7c07012221cdc64a098501c12",
+"assets/fonts/MaterialIcons-Regular.otf": "e4b64d57c177dd465dc39286c4ce0c13",
 "assets/AssetManifest.bin.json": "d782ded6b9354cac7863aa2a2089ac06",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/assets/icons/nuankebao-logo.png": "3f800b529751f432384f4967613ee901",
@@ -35,7 +35,7 @@ const RESOURCES = {"icons/Icon-maskable-192.png": "91221be3ac73e06b2ab3244e9cd31
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
 "version.json": "f1676a0ee7cf8cc9b88ff6ed33968038",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"main.dart.js": "3804aca2023288f4533e33bdd4462627"};
+"main.dart.js": "b876ecc77b840f4067e79ab35879433f"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
