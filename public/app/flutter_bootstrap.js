4b233a02402f697d8334b9736ac74a4208f91e5c
yfxdwc <yfxdwcs@gmail.com>
2026-09-23 05:31:47 +0000

    [pi] 批 3 完成。

--- public/app/flutter_bootstrap.js
+++ public/app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2599946799"
+    serviceWorkerVersion: "2526648477"
   }
 });
