723ba0fb5dc7a8e542fc8183814a98201fc21071
yfxdwc <yfxdwcs@gmail.com>
2026-09-25 16:16:49 +0800

    [SNAPSHOT] task-start: idor-sweep

--- public/app/flutter_bootstrap.js
+++ public/app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2541987372"
+    serviceWorkerVersion: "3670397651"
   }
 });
